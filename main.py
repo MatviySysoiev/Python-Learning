1d3338c5b6e2e7b12f90621469a8defddd76ef6e
Matvii Sysoiev <matviysysoiev2011@icloud.com>
2026-07-20 21:23:27 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
 
-input("Enter your name: ")
+print(name)
+print(age)
+print(city)
+
+print(name.capitalize()) # capitalize делает первую букву заглавной, также переменная name не изменилась
+print(name.upper()) # upper делает все буквы заглавными
+print(dir(name))
