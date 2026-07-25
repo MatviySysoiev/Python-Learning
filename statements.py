de72fd7bbf75b9dbc53938464469fc3afac0900f
Matvii Sysoiev <matviysysoiev2011@icloud.com>
2026-07-25 23:25:17 +0300

    Statements, functions

---
+++ statements.py
@@ -0,0 +1,6 @@
+import datetime
+
+# print(import datetime) doesn't work, because "import datetime" is a statement, not expression
+
+print(datetime.MAXYEAR)
+print(datetime.MINYEAR)
--- testing.py
+++ testing.py
@@ -1,8 +1,16 @@
-#name = 'Hello'
-#print(name.ljust())
+def hello(name):  # name - парамет р
+    print("Hello there,", name, "!")
 
-#print(10, 'Bogdan', True)
-#print(print)
-#print(input)
-#print(dir)
-#print(dir(__builtins__))
+
+# hello('Matthew') #аргумент
+
+def sum_nums(a, b):
+    sum = a+b
+    return (sum)
+    print("This text won't be shown")  # все, что после return не выполняется
+
+
+first_sum = sum_nums(2, 4)
+print(first_sum)
+print(sum_nums(154, 3))
+print(sum_nums(sum_nums(13, 5), 2))
